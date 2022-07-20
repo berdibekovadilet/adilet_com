388efabb6c6035b4c12fd3e420dbc0bc091d95ff
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-20 22:09:47 +0600

    added 404 not found page

---
+++ pages/404.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Button } from "/components/globalComponents/Button";
+import styles from "/styles/PageNotFound.module.scss";
+
+const PageNotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <h1>404</h1>
+        <h2 className={styles.title}>Ой, такой страницы не существует</h2>
+        <p className={styles.desc}>
+          Вы автоматически будете переведны на главную страницу через 5 секунд.
+        </p>
+        <Link href="/">
+          <Button>Перейти на главную самостоятельно</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
